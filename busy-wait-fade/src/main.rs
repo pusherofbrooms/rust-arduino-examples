82eb0b0c501bd75a689eaf21f9ac089ff96e6e80
John Jorgensen <john@2gn.com>
2017-11-19 12:45:48 -0500

    Implelent busy-wait fade

--- busy-wait-fade/src/main.rs
+++ busy-wait-fade/src/main.rs
@@ -1,15 +1,46 @@
-// this is a stub used to create your own rust avr "sketches"
+// use a busy-wait to implement fade.
+//
+// TODO:
+// Use the individual pin instead of changing the whole port
+// The math depends on overruns, which is unintuitive.
+// There is lots of unsafe to wrap in helper functions
+
 #![feature(asm, lang_items, unwind_attributes)]
 
 #![no_std]
 #![no_main]
 
 extern crate arduino;
+extern crate avr_delay;
+
+use arduino::{DDRB, PORTB};
+use core::ptr::write_volatile;
+use avr_delay::{delay_us};
 
 #[no_mangle]
 pub extern fn main() {
-     loop {
-     }
+    let mut out: u8 = 0x00;
+    let mut fade_index: u32 = 1;
+    let mut fade_direction: i32 = 1;
+    unsafe { write_volatile(DDRB, 0xff) }
+
+    loop {
+        if fade_index == 1024 || fade_index == 0 {
+            fade_direction = -fade_direction;
+        }
+        // toggle the port with xor
+        out = out ^ 0xff;
+        // write the value
+        unsafe { write_volatile(PORTB, out) }
+        // delay for a period
+        delay_us(fade_index);
+        out = out ^ 0xff;
+        unsafe { write_volatile(PORTB, out) }
+        // delay for the oposite period as above.
+        delay_us(1024 - fade_index);
+        // This depends on whacky conversion and overrun behavior
+        fade_index += fade_direction as u32;
+    }
 }
 
 // std stubs. A PR in the arduino project may eliminate the need for this.
