ac43985d5f229d958df349a3dadd51a113bd38bf
John Jorgensen <john@2gn.com>
2017-12-01 15:31:23 -0500

    Fix Cargo.toml for new avr_delay home

---
+++ blink/upload.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# set the port to whatever your arduino board uses
+PORT=/dev/ttyACM0
+
+avrdude -c arduino -p atmega328p -P $PORT -U flash:w:target/avr-atmega328p/release/dlyblink.elf
+
